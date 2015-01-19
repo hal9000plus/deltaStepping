86060709125a99903f613965fd7456cb90bef160
hal9000 <hal9000plus@users.noreply.github.com>
2015-01-19 17:27:07 +0200

    Update delta_stepping.c
    in progress

--- src/delta_stepping.c
+++ src/delta_stepping.c
@@ -17,6 +17,7 @@
 #include <stddef.h>
 #define NEWARRAY(x, n) do { (x) = calloc((n), sizeof *(x)); } while (0)
 #define RESIZEARRAY(x, n) do { (x) = realloc((x), sizeof *(x) * (n) ); } while (0)
+float precision = 0.00001;
 
 //the graph will be represented as an array of nodes
 typedef struct NODE{
@@ -30,10 +31,18 @@ typedef struct NODE{
 typedef struct BUCKETELEMENT{
 	double* tentDist;//-1.0 denotes infinite distance  //-2.0 just initialized
 	int* nodeIds;
+	int* bucket2Index;//helper to find the element in constant time
 	int maxNodes;
 	int numberOfNodes;
 	int index;
 } BUCKETELEMENT;
+
+typedef struct REQUESTS{
+	int *targetId;
+	int numberOfelements;
+	double *weightSourceToTarget;
+	double tendDistanceOfSource;
+}REQUESTS;
 
 NODE* New_grapg(int numberOfNodes){
 	NODE *newGraph= calloc(numberOfNodes, sizeof(NODE));
@@ -51,12 +60,57 @@ BUCKETELEMENT* New_Bucket(int numberOfBuckets){
 		assert(newBucket!=NULL);
 	int i=0;
 	for(i=0;i<numberOfBuckets;i++){
-		newBucket->index=-2;//-2 just initialized
-		newBucket->numberOfNodes=0;
+		(newBucket+i)->index=-2;//-2 just initialized
+		(newBucket+i)->numberOfNodes=0;
 	}
 	return newBucket;
 }
 
+BUCKETELEMENT* resizeBucket(BUCKETELEMENT* bucketArray,int newSize,int oldSize){
+	RESIZEARRAY(bucketArray,newSize);
+		assert(bucketArray!=NULL);
+	int i=0;
+	if(oldSize<newSize){
+		for(i=oldSize;i<newSize;i++){
+			(bucketArray+i)->index=-2;//-2 just initialized
+			(bucketArray+i)->numberOfNodes=0;
+		}
+	}
+	return bucketArray;
+}
+
+void buildRequests(NODE aNode,REQUESTS *lightEdgesRequests,double delta,double tendDistSource){
+	int neigboursSize=aNode.numberOfEdges;
+	int kk=0;
+	int innerCounter=0;
+	if(lightEdgesRequests->targetId==NULL||lightEdgesRequests->weightSourceToTarget==NULL){
+		NEWARRAY(lightEdgesRequests->targetId,neigboursSize);//at max is neigboursSize
+		NEWARRAY(lightEdgesRequests->weightSourceToTarget,neigboursSize);
+		lightEdgesRequests->numberOfelements=0;
+	}else{
+		RESIZE(lightEdgesRequests->targetId,lightEdgesRequests->numberOfelements+neigboursSize);
+		RESIZE(lightEdgesRequests->weightSourceToTarget,lightEdgesRequests->numberOfelements+neigboursSize);
+	}
+	lightEdgesRequests->tendDistanceOfSource=tendDistSource;
+	for(kk=0 ; kk < neigboursSize ; kk++ ){
+		if( *((aNode.weigthsList)+kk) <= delta ){
+			*(lightEdgesRequests->(targetId+innerCounter+lightEdgesRequests->numberOfelements))=*(aNode.edgeList+kk);
+			*(lightEdgesRequests->(weightSourceToTarget+innerCounter+lightEdgesRequests->numberOfelements)=*((aNode.weigthsList)+kk);
+			lightEdgesRequests->(numberOfelements++);
+			innerCounter++;
+		}
+	}
+}
+
+int isBucketEmpty(BUCKETELEMENT *aBucketElement){
+	if(aBucketElement->numberOfNodes==0 || aBucketElement==NULL){
+		return 1;
+	}else{
+		return 0;
+	}
+}
+
+int
 /* rangefrom inclusive end not inclusive */
 int get_randomNumber(int rangefrom,int rangeTo)
 {
@@ -80,11 +134,12 @@ void pushEdge(NODE *anode,int edgeId,double edgeWeight){
 	anode->numberOfEdges++;
 }
 
-void pushDistanceAndNode(BUCKETELEMENT *bucketarray, int nodeId, double distance){
+void pushDistanceAndNode(BUCKETELEMENT *bucketarray, int nodeId, double distance,int index){
 
 	if(bucketarray->nodeIds==NULL){
 		NEWARRAY(bucketarray->nodeIds,5);
 		NEWARRAY(bucketarray->tentDist,5);
+		NEWARRAY(bucketarray->bucket2Index,5);
 		bucketarray->maxNodes=5;
 	}
 
@@ -93,43 +148,65 @@ void pushDistanceAndNode(BUCKETELEMENT *bucketarray, int nodeId, double distance
 		bucketarray->maxNodes=bucketarray->maxNodes*2;
 		RESIZEARRAY((bucketarray->nodeIds),bucketarray->maxNodes);
 		RESIZEARRAY((bucketarray->tentDist),bucketarray->maxNodes);
+		RESIZEARRAY((bucketarray->bucket2Index),bucketarray->maxNodes);
 	}
 	*(bucketarray->nodeIds+bucketarray->numberOfNodes)=nodeId;
 	*(bucketarray->tentDist+bucketarray->numberOfNodes)=distance;
+	*(bucketarray->bucket2Index+bucketarray->numberOfNodes)=index;
 	bucketarray->numberOfNodes++;
 }
 
-void popDistanceAndNode(BUCKETELEMENT *bucketarray, int nodeId, double distance){
+void popDistanceAndNode(BUCKETELEMENT *bucketarray, int nodeId){
 	if(bucketarray->nodeIds==NULL){
 		return;
 	}
+	short foundFlag=0;
 	int* tempNodeIds = calloc((bucketarray->maxNodes - 1), sizeof(int)); // allocate an array with a size 1 less than the current one
 	double* tempDistances = calloc((bucketarray->maxNodes - 1), sizeof(double)); // allocate an array with a size 1 less than the current one
+	int* tempBucket2Index = calloc((bucketarray->maxNodes - 1), sizeof(double));
 	int i=0;
 	int j=0;
 	for(i=0; i<bucketarray->numberOfNodes; i++){
 		if(*(bucketarray->nodeIds+i)!=nodeId){
-
-			*(tempNodeIds+j)=nodeId;
-			*(tempDistances+j)=distance;
+			*(tempNodeIds+j)=*(bucketarray->nodeIds+i);
+			*(tempDistances+j)=*(bucketarray->tentDist+i);
+			*(tempBucket2Index+j)=*(bucketarray->bucket2Index+i);
 			j++;
+		}else{
+			foundFlag=1;
 		}
 	}
-	memcpy(bucketarray->nodeIds, tempNodeIds, ((bucketarray->maxNodes - 1)) * sizeof(int));
-	memcpy(bucketarray->tentDist, tempDistances, ((bucketarray->maxNodes - 1)) * sizeof(int));
-	bucketarray->numberOfNodes--;
+	if(foundFlag){
+		memcpy(bucketarray->nodeIds, tempNodeIds, ((bucketarray->maxNodes - 1)) * sizeof(int));
+		memcpy(bucketarray->tentDist, tempDistances, ((bucketarray->maxNodes - 1)) * sizeof(int));
+		bucketarray->numberOfNodes--;
 
-	int N = bucketarray->numberOfNodes;
-	int totalSize = bucketarray->maxNodes;
+		int N = bucketarray->numberOfNodes;
+		int totalSize = bucketarray->maxNodes;
 
-	if (N > 0 && N == totalSize/4){
-		bucketarray->maxNodes = totalSize/2;
-		RESIZEARRAY((bucketarray->nodeIds),bucketarray->maxNodes);
-		RESIZEARRAY((bucketarray->tentDist),bucketarray->maxNodes);
+		if (N > 0 && N <= totalSize/4){
+			bucketarray->maxNodes = totalSize/2;
+			RESIZEARRAY((bucketarray->nodeIds),bucketarray->maxNodes);
+			RESIZEARRAY((bucketarray->tentDist),bucketarray->maxNodes);
+		}
 	}
 }
 
+double findTargetDistance(BUCKETELEMENT *locBucketArray,int nodeId,int sizeOfBucketArray,int *targetNodeBucketIndex,int *targetNodeIndexInBucket){
+	int ii=0;
+	int ee=0;
+	for(ii=0; ii<sizeOfBucketArray ;ii++ ){
+		for(ee=0 ; ee < (locBucketArray+ii)->numberOfNodes ; ee++){
+			int currentNode=*((locBucketArray+ii)->nodeIds+ee);
+			if(currentNode==nodeId){
+				*targetNodeBucketIndex=ii;
+				*targetNodeIndexInBucket=ee;
+				return (locBucketArray+ii)->tentDist;
+			}
+		}
+	}
 
+}
 void readEdges(NODE* graph,const char *line){
 	char *token;
 	short counter=0;
@@ -194,6 +271,15 @@ main(int argc, char *argv[])
 	BUCKETELEMENT *localBucketArray;
 	NEWARRAY(sizeOfGraph,1);
 	memset( tempCountsPerProcess, 0, num_procs*sizeof(int) );//for c99 compiler
+	int sourceNodeId;
+	int currentBucketIndex;
+	REQUESTS *requestsArray;
+	int *removed;
+	double delta;
+	int *gatherCountsSendBuffer=NULL;
+	int *gatherCountsRcvBuffer=NULL;
+	int gatherMax=0;
+	int localBucketSize=0;
 	/*end of declarations and initializations*/
 
 	if(my_rank==0){
@@ -202,7 +288,8 @@ main(int argc, char *argv[])
 			exit(1);
 		}
 		nameOfGraphFile=argv[1];
-		double delta=atof(argv[2]);
+		delta=atof(argv[2]);
+		sourceNodeId=atoi(argv[3]);
 		if(nameOfGraphFile!=NULL){
 			fr = fopen (nameOfGraphFile, "r");  /* open the file for reading */
 			   /* elapsed.dta is the name of the file */
@@ -239,7 +326,7 @@ main(int argc, char *argv[])
 		memset( countsPerProcess, 0, (num_procs-1)*sizeof(int) );//for c99 compiler
 		NEWARRAY(ind,*(sizeOfGraph)-1);
 		for(counter=0;counter<*sizeOfGraph;counter++){
-			ind[counter]=get_randomNumber(0,num_procs);//generate the randomly assignments of nodes to processes
+			ind[counter]=get_randomNumber(0,num_procs);//generate the randomly assignments of nodes to processes u
 			countsPerProcess[ind[counter]]++;
 		}
 		maxSize=0;
@@ -251,8 +338,8 @@ main(int argc, char *argv[])
 		sizeOfScatteredList= maxSize*num_procs;
 		globalScatteredList=New_grapg(sizeOfScatteredList);
 		for(counter=0;counter<*sizeOfGraph;counter++){
-			*(globalScatteredList+maxSize*(ind[counter]-1)+tempCountsPerProcess[(ind[counter]-1)])=*(graph+counter);
-			tempCountsPerProcess[(ind[counter]-1)]++;
+			*(globalScatteredList+maxSize*(ind[counter])+tempCountsPerProcess[(ind[counter])])=*(graph+counter);
+			tempCountsPerProcess[(ind[counter])]++;
 		}
 		printf("in here \n");
 	}
@@ -277,6 +364,26 @@ main(int argc, char *argv[])
 	MPI_Type_create_struct(nitems, blocklengths, offsets, types, &mpi_local_counts_type);
 	MPI_Type_commit(&mpi_local_counts_type);
 	/* end of new datatype */
+
+	/* Declaration of new datatype */
+	const int nitems2=3;
+	int          blocklengths2[3] = {1,1,1};
+	MPI_Datatype types2[3] = {MPI_INT, MPI_INT,MPI_DOUBLE};
+	MPI_Datatype mpi_request_type;
+	MPI_Aint     offsets2[3];
+	MPI_Aint 	 addr2[3];
+	MPI_Get_address(requestsArray, &addr2[0]);
+	MPI_Get_address(&requestsArray->targetId, &addr2[1]);
+	MPI_Get_address(&requestsArray->processIdOfTarget, &addr2[2]);
+	MPI_Get_address(&requestsArray->weightSourceToTarget, &addr2[3]);
+	offsets2[0] = addr2[1] - addr2[0];
+	offsets2[1] = addr2[2] - addr2[0];
+	offsets2[2] = addr2[3] - addr2[0];
+	MPI_Type_create_struct(nitems2, blocklengths2, offsets2, types2, &mpi_request_type);
+	MPI_Type_commit(&mpi_request_type);
+		/* end of new datatype */
+
+
 	printf("before bcast my_rank=%d \n",my_rank);
 	MPI_Barrier(MPI_COMM_WORLD);
 	MPI_Bcast(sizeOfGraph,1,MPI_INT,0,MPI_COMM_WORLD);
@@ -289,6 +396,7 @@ main(int argc, char *argv[])
 		assert(ind  != NULL);
 	MPI_Bcast(tempCountsPerProcess,num_procs-1,MPI_INT,0,MPI_COMM_WORLD);
 	MPI_Bcast(ind,(*sizeOfGraph)-1,MPI_INT,0,MPI_COMM_WORLD);
+	MPI_Bcast(&delta,1,MPI_DOUBLE,0,MPI_COMM_WORLD);
 	printf("after 2 bcast my_rank=%d \n",my_rank);
 	MPI_Bcast(&sizeOfScatteredList,1,MPI_INT,0,MPI_COMM_WORLD);
 	printf("after 3 bcast my_rank=%d \n",my_rank);
@@ -300,33 +408,98 @@ main(int argc, char *argv[])
 	printf("after SCATTER my_rank=%d \n",my_rank);
 
 	localBucketArray=New_Bucket(2);//initial we start with only the 0 bucket and the infinite distance bucket
-	(localBucketArray+1)->index=-1;//go to the bucket that holds infinite tentative distances
-	(localBucketArray)->index=0;//go to the bucket that holds the source node
+	localBucketSize=2;
+	(localBucketArray)->index=-1;//go to the bucket that holds infinite
+	(localBucketArray)->numberOfNodes=0;
+	(localBucketArray+1)->index=0;//go to the bucket that holds source
+	(localBucketArray+1)->numberOfNodes=0;
+
 	/* fill the all the local nodes to the infinite bucket*/
 	int ll=0;
 	for(ll=0;ll<tempCountsPerProcess[my_rank];ll++){
-		pushDistanceAndNode(localBucketArray+1,(localScatteredReceiveList+ll)->nodeId,-2.0);
-	}
-
-	///send the buckets ??or send the nodes??
-	/*
-	if(*sizeOfGraph%num_procs==0){
-		localScatteredReceiveList=New_grapg(*sizeOfGraph/num_procs);
-	}else{
-		//to do
-		printf("sizeOfGraph%num_procs==0 \n");
-		exit(1);
-	}
-
-
-	int ii=0;
-	int localCounter=0;
-	for(ii=1;ii<num_procs;ii++){
-		if(my_rank==ind[ii]){
-
+		if(((localScatteredReceiveList+ll)->nodeId)==sourceNodeId){
+			printf("found source node on process %d",my_rank);
+			pushDistanceAndNode(localBucketArray+1,(localScatteredReceiveList+ll)->nodeId,0.0);
+		}else{
+			pushDistanceAndNode(localBucketArray,(localScatteredReceiveList+ll)->nodeId,-1.0);
 		}
 	}
+	currentBucketIndex=1;//the infinite bucket is the bucket array element 0;
+	/*create requests */
+
+	/*allocate variables */
+	int sourceNodesRequests[(localBucketArray+currentBucketIndex)->numberOfNodes];
+	REQUESTS *requestsToSendList;
+	REQUESTS *requestsToReceive;
+	NEWARRAY(requestsToSendList,1);
+	requestsToSendList->targetId=NULL;
+	requestsToSendList->weightSourceToTarget=NULL;
+	NEWARRAY(requestsToReceive,num_procs-1);///
+	requestsToReceive->targetId=NULL;
+	requestsToReceive->weightSourceToTarget=NULL;
+	int *removedNodesList;
+
+	NEWARRAY(removedNodesList,localBucketArray->numberOfNodes);
+	/*end of allocate variables */
+	/* iterate all the nodes the bucket */
+	for(ll=0;ll<(localBucketArray+currentBucketIndex)->numberOfNodes;ll++){
+		//iterate all the edges
+		int nodeToProcess=*((localBucketArray+currentBucketIndex)->nodeIds+ll);
+		int indexInScatteredList=*((localBucketArray+currentBucketIndex)->bucket2Index+ll);
+		double sourceTentDist=*((localBucketArray+currentBucketIndex)->tentDist+ll);
+		buildRequests(nodeToProcess,requestsToSendList,delta,sourceTentDist);
+		*(removedNodesList+ll)=nodeToProcess;
+	}
+	/*end of iterate all the nodes the bucket */
+
+	/*
+	NEWARRAY(gatherCountsSendBuffer,1);//create the gather buffer
+	if(my_rank==0){
+		NEWARRAY(gatherCountsRcvBuffer,num_procs-1);
+	}
+
+
+	MPI_Gather(gatherCountsSendBuffer,1,MPI_INT,gatherCountsRcvBuffer,1,MPI_INT,0,MPI_COMM_WORLD);//gather the requests size in root in order to specify the max size for the allgather
+	if(my_rank==0){
+		int ll=0;
+		for(ll=0;ll<num_procs-1;ll++){
+			if(gatherMax < gatherCountsRcvBuffer[ll]){
+				gatherMax = gatherCountsRcvBuffer[ll];
+			}
+		}
+	}
+	MPI_Bcast(&gatherMax,1,MPI_INT,0,MPI_COMM_WORLD);
+	RESIZEARRAY(requestsToSendList,gatherMax);
 	*/
+
+	MPI_Allgather(requestsToSendList,1,mpi_request_type,requestsToReceive,1,mpi_request_type,MPI_COMM_WORLD)
+	int tempOuterCounter=0;
+	int tempInnerCounter=0;
+	/* all processes perform Relaxations */
+
+	for( tempOuterCounter=0 ; tempOuterCounter < num_procs-1; tempOuterCounter++){
+		for(tempInnerCounter=0 ; tempInnerCounter < (requestsToReceive+tempOuterCounter)->numberOfelements ; tempInnerCounter){
+			int currentTargeNodeId=*( (requestsToReceive+tempOuterCounter)->targetId+tempInnerCounter )
+			if( ind[currentTargeNodeId]==my_rank ){//if true the the node must be someware in the bucket O(n) time for searching ///or improvement we could use a hashSet ..
+				int targetNodeBucketIndex=0;
+				int targeNodeIndexInsideBucket=0;
+				double tentDistSource=(requestsToReceive+tempOuterCounter)->tendDistanceOfSource;
+				double tentDistTarget=findTargetDistance(localBucketArray,currentTargeNodeId,localBucketSize,&targetNodeBucketIndex,&targeNodeIndexInsideBucket);
+				double sourceToTargetWeight=*((requestsToReceive+tempOuterCounter)->weightSourceToTarget+tempInnerCounter);
+				if(tentDistSource+sourceToTargetWeight < tentDistTarget){
+					//(localBucketArray+targetNodeBucketIndex)
+					popDistanceAndNode(localBucketArray+targetNodeBucketIndex);
+					int arrayIndexToInsertNeWdDist=( (tentDistSource+sourceToTargetWeight)/(int)delta)+1;
+
+					pushDistanceAndNode(localBucketArray+)
+				}
+				requestsToSendList->targetId
+			}
+		}
+	}
+
+
+
 
 	/* shut down MPI */
 	MPI_Barrier(MPI_COMM_WORLD);
@@ -334,3 +507,4 @@ main(int argc, char *argv[])
 
 	return 0;
 }
+
